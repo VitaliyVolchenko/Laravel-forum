0a385cca5a5cf36c35b327ca0e38d74e0adda313
vitaliyvolchenko <vitaliyvolchenkodeveloper@gmail.com>
2018-09-28 16:42:43 +0300

    Extracting to View Composers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // \view()->composer('*', function ($view) {
+        //     $view->with('channels', \App\Channel::all());
+        // });
+        \View::share('channels', \App\Channel::all());
     }
 
     /**
