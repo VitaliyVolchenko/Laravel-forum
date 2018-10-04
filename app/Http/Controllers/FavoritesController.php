16e18cd1429390e085ae8bb4510a41c63bef1f7f
vitaliyvolchenko <vitaliyvolchenkodeveloper@gmail.com>
2018-10-04 12:37:26 +0300

    A User Can Favorite Replies

---
+++ app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Favorite;
+use App\Reply;
+
+class FavoritesController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Store a new favorite in the database.
+     *
+     * @param  Reply $reply
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function store(Reply $reply)
+    {
+        return $reply->favorite();
+
+        //return $reply->favorites()->create(['user_id' => auth()->id() ]);    
+
+        // Favorite::create([
+        //         'user_id' => auth()->id(),
+        //         'favorited_id' => $reply->id,
+        //         'favorited_type' => get_class($reply)
+        // ]);
+
+        //  \DB::table('favorites')->insert([
+        //     'user_id' => auth()->id(),
+        //     'favorited_id' => $reply->id,
+        //     'favorited_type' => get_class($reply)
+        // ]);
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,5 @@ Route::get('/threads/create', 'ThreadsController@create');
 Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
 Route::post('/threads', 'ThreadsController@store');
 Route::get('/threads/{channel}', 'ThreadsController@index');
-
-//Route::resource('/threads', 'ThreadsController');
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
-
+Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
