80bc303da354bb586a5fd569a98b45e0ce6b0f15
vitaliyvolchenko <vitaliyvolchenkodeveloper@gmail.com>
2018-10-04 14:25:55 +0300

    The Favorite Button

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -23,7 +23,9 @@ class FavoritesController extends Controller
      */
     public function store(Reply $reply)
     {
-        return $reply->favorite();
+         $reply->favorite();
+
+         return back();
 
         //return $reply->favorites()->create(['user_id' => auth()->id() ]);    
 
--- app/Reply.php
+++ app/Reply.php
@@ -25,4 +25,12 @@ class Reply extends Model
             return $this->favorites()->create($attributes);
         }
     }
+
+    public function isFavorited()
+    {
+        
+        return $this->favorites()->where('user_id', auth()->id())->exists();
+        
+    }
+
 }
